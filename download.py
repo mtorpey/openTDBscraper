175ae60b50e114da13fb2b6db99e1c9ab1d84ccb
Adrien Cosson <ad.cosson@gmail.com>
2018-04-20 04:39:12 +1000

    Added multiprocessing

--- download.py
+++ download.py
@@ -4,29 +4,32 @@ import json
 from urllib.request import urlopen
 from random import shuffle
 from math import floor
+from multiprocessing import Pool
 
 
-nbQuestions = 250
+nbQuestions = 500
 nbBatches = floor(nbQuestions / 50)
 categories = [e for e in range(50)]
 
 # Generate a token
 response = urlopen("https://opentdb.com/api_token.php?command=request")
 html = response.read()
-token = json.loads(html)["token"]
-print("Using session token: " + token)
+session_token = json.loads(html)["token"]
+print("Using session token: " + session_token)
 
-# Loop on every category
-for category in categories:
-    print("\nWorking on category %d" % category)
-    sourceurl = 'https://opentdb.com/api.php?amount=50&category=' + str(category)
+
+def download_category(category, token=session_token):
+    print("Working on category %d" % category)
+    sourceurl = 'https://opentdb.com/api.php?amount=50&category='\
+         + str(category) + "&token=" + token
     file = open("questions%d.csv" % category, "w")
 
     # Loop on the number of batches wanted
     for i in range(nbBatches):
-        print("Downloading questions %d-%d out of %d" % (i*50, (i+1)*50, nbQuestions))
+        print("Downloading questions %d-%d out of %d for category %d"\
+             % (i*50, (i+1)*50, nbQuestions, category))
         # Download the questions/answers
-        response = urlopen(sourceurl + "&token=" + token)
+        response = urlopen(sourceurl)
         html = response.read()
         questions = json.loads(html)["results"]
 
@@ -45,3 +48,7 @@ for category in categories:
             line += ";" + q["correct_answer"].replace(";", '') + "\n"
             file.write(line)
     file.close()
+
+if __name__ == "__main__":
+    with Pool(len(categories)) as p:
+        p.map(download_category, categories)
