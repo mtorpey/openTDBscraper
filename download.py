6b7c3566f33013bc1e58d116a66b8126e77c542f
Adrien Cosson <ad.cosson@gmail.com>
2018-04-20 04:28:29 +1000

    Download multiple categories at once

--- download.py
+++ download.py
@@ -5,9 +5,10 @@ from urllib.request import urlopen
 from random import shuffle
 from math import floor
 
-sourceurl = 'https://opentdb.com/api.php?amount=50&category=23'
-nbQuestions = 500
-file = open("questions.csv", "w")
+
+nbQuestions = 250
+nbBatches = floor(nbQuestions / 50)
+categories = [e for e in range(50)]
 
 # Generate a token
 response = urlopen("https://opentdb.com/api_token.php?command=request")
@@ -15,26 +16,32 @@ html = response.read()
 token = json.loads(html)["token"]
 print("Using session token: " + token)
 
-# Loop for  while
-for i in range(floor(nbQuestions / 50)):
-    print("Downloading questions %d-%d out of %d" % (i*50, (i+1)*50, nbQuestions))
-    # Download the questions/answers
-    response = urlopen(sourceurl + "&token=" + token)
-    html = response.read()
-    questions = json.loads(html)["results"]
+# Loop on every category
+for category in categories:
+    print("\nWorking on category %d" % category)
+    sourceurl = 'https://opentdb.com/api.php?amount=50&category=' + str(category)
+    file = open("questions%d.csv" % category, "w")
 
-    # Process the questions/answers
-    for q in questions:
-        line = ""
-        if q["type"] == "boolean":
-            line += "True or false?" + ";" + q["question"].replace(";", '')
-        else:
-            line += q["question"].replace(";", '') + ";"
-            answers = [q["correct_answer"]] + q["incorrect_answers"]
-            shuffle(answers)
-            for answer in answers:
-                line += answer.replace(";", '') + "<br>"
-            line = line[:-4]
-        line += ";" + q["correct_answer"].replace(";", '') + "\n"
-        file.write(line)
-file.close()
+    # Loop on the number of batches wanted
+    for i in range(nbBatches):
+        print("Downloading questions %d-%d out of %d" % (i*50, (i+1)*50, nbQuestions))
+        # Download the questions/answers
+        response = urlopen(sourceurl + "&token=" + token)
+        html = response.read()
+        questions = json.loads(html)["results"]
+
+        # Process the questions/answers
+        for q in questions:
+            line = ""
+            if q["type"] == "boolean":
+                line += "True or false?" + ";" + q["question"].replace(";", '')
+            else:
+                line += q["question"].replace(";", '') + ";"
+                answers = [q["correct_answer"]] + q["incorrect_answers"]
+                shuffle(answers)
+                for answer in answers:
+                    line += answer.replace(";", '') + "<br>"
+                line = line[:-4]
+            line += ";" + q["correct_answer"].replace(";", '') + "\n"
+            file.write(line)
+    file.close()
