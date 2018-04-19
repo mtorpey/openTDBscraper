78d3ed371083e0278148bd367c05f7af3163f0c0
Adrien Cosson <ad.cosson@gmail.com>
2018-04-20 04:57:08 +1000

    Optimied the downloading process and auto-deletion of empty files

--- download.py
+++ download.py
@@ -1,6 +1,7 @@
 #! /usr/bin/python3
 
 import json
+import os
 from urllib.request import urlopen
 from random import shuffle
 from math import floor
@@ -9,7 +10,7 @@ from multiprocessing import Pool
 
 nbQuestions = 2000
 nbBatches = floor(nbQuestions / 50)
-categories = [e for e in range(9,33)]
+categories = [e for e in range(1,33)]
 
 # Generate a token
 response = urlopen("https://opentdb.com/api_token.php?command=request")
@@ -23,6 +24,7 @@ def download_category(category, token=session_token):
     sourceurl = 'https://opentdb.com/api.php?amount=50&category='\
          + str(category) + "&token=" + token
     file = open("questions%d.csv" % category, "w")
+    nbLines = 0
 
     # Loop on the number of batches wanted
     for i in range(nbBatches):
@@ -33,21 +35,27 @@ def download_category(category, token=session_token):
         html = response.read()
         questions = json.loads(html)["results"]
 
-        # Process the questions/answers
-        for q in questions:
-            line = ""
-            if q["type"] == "boolean":
-                line += "True or false?" + ";" + q["question"].replace(";", '')
-            else:
-                line += q["question"].replace(";", '') + ";"
-                answers = [q["correct_answer"]] + q["incorrect_answers"]
-                shuffle(answers)
-                for answer in answers:
-                    line += answer.replace(";", '') + "<br>"
-                line = line[:-4]
-            line += ";" + q["correct_answer"].replace(";", '') + "\n"
-            file.write(line)
+        if questions:
+            # Process the questions/answers
+            for q in questions:
+                line = ""
+                if q["type"] == "boolean":
+                    line += "True or false?" + ";" + q["question"].replace(";", '')
+                else:
+                    line += q["question"].replace(";", '') + ";"
+                    answers = [q["correct_answer"]] + q["incorrect_answers"]
+                    shuffle(answers)
+                    for answer in answers:
+                        line += answer.replace(";", '') + "<br>"
+                    line = line[:-4]
+                line += ";" + q["correct_answer"].replace(";", '') + "\n"
+                file.write(line)
+                nbLines += 1
+        else: 
+            break
     file.close()
+    if nbLines == 0:
+        os.remove("questions%d.csv" % category)
 
 if __name__ == "__main__":
     with Pool(len(categories)) as p:
