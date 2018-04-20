77d0195c56334170f5e3e2c47ca00436704b9724
Adrien Cosson <ad.cosson@gmail.com>
2018-04-20 17:14:19 +1000

    Added an API call to get the list of all categories

--- download.py
+++ download.py
@@ -12,7 +12,12 @@ nbQuestions = 1000
 nbQuestionsPerBatches = 20
 nbBatches = floor(nbQuestions / nbQuestionsPerBatches)
 #TODO: Query https://opentdb.com/api_category.php to get the exact list of categories and store the id/name pair in a dict/list 
-categories = [e for e in range(1,33)]
+# categories = [e for e in range(1,33)]
+
+# Get the list of all categories
+response = urlopen("https://opentdb.com/api_category.php")
+html = response.read()
+categories = json.loads(html)["trivia_categories"]
 
 # Generate a token
 response = urlopen("https://opentdb.com/api_token.php?command=request")
@@ -24,48 +29,42 @@ print("Using session token: " + session_token)
 def download_category(category, token=session_token):
     #TODO: Query https://opentdb.com/api_count.php?category=CATEGORY to get the number of questions for the category
     # and find the largest divisor of it under 50 to make sure we get all the questions
-    print("Working on category %d" % category)
-    # Get the category name
-    categoryurl = 'https://opentdb.com/api.php?amount=1&category=%d' % category
+    print("Working on category %d" % category["id"])
     sourceurl = 'https://opentdb.com/api.php?amount=%d&category=%d&token=%s'\
-         % (nbQuestionsPerBatches, category, token)
-    response = urlopen(categoryurl)
-    html = response.read()
-    results = json.loads(html)["results"]
-    if results:
-        category_name = results[0]["category"]
-        # Create the category file
-        filename = "%s.csv" % category_name
-        file = open(filename, "w")
-        # Loop on the number of batches wanted
-        for i in range(nbBatches):
-            print("Downloading questions %d-%d out of %d for category %d (%s)"\
-                % (i*nbQuestionsPerBatches, (i+1)*nbQuestionsPerBatches, nbQuestions,\
-                 category, category_name))
-            # Download the questions/answers
-            response = urlopen(sourceurl)
-            html = response.read()
-            questions = json.loads(html)["results"]
+         % (nbQuestionsPerBatches, category["id"], token)
+    category_name = category["name"]
+    # Create the category file
+    filename = "%s.csv" % category_name
+    file = open(filename, "w")
+    # Loop on the number of batches wanted
+    for i in range(nbBatches):
+        print("Downloading questions %d-%d out of %d for category %d (%s)"\
+            % (i*nbQuestionsPerBatches, (i+1)*nbQuestionsPerBatches, nbQuestions,\
+                category["id"], category_name))
+        # Download the questions/answers
+        response = urlopen(sourceurl)
+        html = response.read()
+        questions = json.loads(html)["results"]
 
-            if questions:
-                # Process the questions/answers
-                for q in questions:
-                    line = ""
-                    if q["type"] == "boolean":
-                        line += "True or false?" + ";" + q["question"].replace(";", '')
-                    else:
-                        line += q["question"].replace(";", '') + ";"
-                        answers = [q["correct_answer"]] + q["incorrect_answers"]
-                        shuffle(answers)
-                        for answer in answers:
-                            line += answer.replace(";", '') + "<br>"
-                        line = line[:-4]
-                    line += ";" + q["correct_answer"].replace(";", '') + "\n"
-                    file.write(line)
-            else: 
-                # No more questions to download
-                break
-        file.close()
+        if questions:
+            # Process the questions/answers
+            for q in questions:
+                line = ""
+                if q["type"] == "boolean":
+                    line += "True or false?" + ";" + q["question"].replace(";", '')
+                else:
+                    line += q["question"].replace(";", '') + ";"
+                    answers = [q["correct_answer"]] + q["incorrect_answers"]
+                    shuffle(answers)
+                    for answer in answers:
+                        line += answer.replace(";", '') + "<br>"
+                    line = line[:-4]
+                line += ";" + q["correct_answer"].replace(";", '') + "\n"
+                file.write(line)
+        else: 
+            # No more questions to download
+            break
+    file.close()
 
 if __name__ == "__main__":
     nbProcesses = len(categories)
