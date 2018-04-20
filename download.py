7ca023ae6ef686e54ed48a7c34d472b3e96432d4
Adrien Cosson <ad.cosson@gmail.com>
2018-04-20 17:38:48 +1000

    Optimized the execution time by optimizing the size of the batches

--- download.py
+++ download.py
@@ -8,11 +8,9 @@ from math import floor
 from multiprocessing import Pool
 
 
-nbQuestions = 1000
-nbQuestionsPerBatches = 20
-nbBatches = floor(nbQuestions / nbQuestionsPerBatches)
-#TODO: Query https://opentdb.com/api_category.php to get the exact list of categories and store the id/name pair in a dict/list 
-# categories = [e for e in range(1,33)]
+# nb_questions = 1000
+nb_questions_per_batches = 50
+# nb_batches = floor(nb_questions / nb_questions_per_batches)
 
 # Get the list of all categories
 response = urlopen("https://opentdb.com/api_category.php")
@@ -27,25 +25,36 @@ print("Using session token: " + session_token)
 
 
 def download_category(category, token=session_token):
-    #TODO: Query https://opentdb.com/api_count.php?category=CATEGORY to get the number of questions for the category
-    # and find the largest divisor of it under 50 to make sure we get all the questions
     print("Working on category %d" % category["id"])
+
+    # Get the number of questions for this category
+    response = urlopen("https://opentdb.com/api_count.php?category=%d"\
+         % category["id"])
+    html = response.read()
+    nb_questions = json.loads(html)["category_question_count"]["total_question_count"]
+
+    # Calculate the number of batches and the number of remaining questions
+    nb_batches = floor(nb_questions / nb_questions_per_batches)
+    remaining_questions = nb_questions % nb_questions_per_batches
+
+    # Create the request URL
     sourceurl = 'https://opentdb.com/api.php?amount=%d&category=%d&token=%s'\
-         % (nbQuestionsPerBatches, category["id"], token)
-    category_name = category["name"]
+         % (nb_questions_per_batches, category["id"], token)
+
     # Create the category file
+    category_name = category["name"]
     filename = "%s.csv" % category_name
     file = open(filename, "w")
+
     # Loop on the number of batches wanted
-    for i in range(nbBatches):
+    for i in range(nb_batches):
         print("Downloading questions %d-%d out of %d for category %d (%s)"\
-            % (i*nbQuestionsPerBatches, (i+1)*nbQuestionsPerBatches, nbQuestions,\
+            % (i*nb_questions_per_batches + 1, (i+1)*nb_questions_per_batches, nb_questions,\
                 category["id"], category_name))
         # Download the questions/answers
         response = urlopen(sourceurl)
         html = response.read()
         questions = json.loads(html)["results"]
-
         if questions:
             # Process the questions/answers
             for q in questions:
@@ -64,7 +73,34 @@ def download_category(category, token=session_token):
         else: 
             # No more questions to download
             break
+
+    # Create the URL for the remaining questions
+    sourceurl = 'https://opentdb.com/api.php?amount=%d&category=%d&token=%s'\
+        % (remaining_questions, category["id"], token)
+    print("Downloading questions %d-%d out of %d for category %d (%s)"\
+        % (nb_questions - remaining_questions + 1, nb_questions, nb_questions,\
+            category["id"], category_name))
+    # Download the questions/answers
+    response = urlopen(sourceurl)
+    html = response.read()
+    questions = json.loads(html)["results"]
+    # Process the questions/answers
+    for q in questions:
+        line = ""
+        if q["type"] == "boolean":
+            line += "True or false?" + ";" + q["question"].replace(";", '')
+        else:
+            line += q["question"].replace(";", '') + ";"
+            answers = [q["correct_answer"]] + q["incorrect_answers"]
+            shuffle(answers)
+            for answer in answers:
+                line += answer.replace(";", '') + "<br>"
+            line = line[:-4]
+        line += ";" + q["correct_answer"].replace(";", '') + "\n"
+        file.write(line)
+
     file.close()
+
 
 if __name__ == "__main__":
     nbProcesses = len(categories)
