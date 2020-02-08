2f4ea5e05da15e8cd5e38cc3b16ffbed700eaa63
Michael Torpey <mct25@st-andrews.ac.uk>
2020-02-08 16:10:21 +0000

    Sort numbering of questions

--- download.py
+++ download.py
@@ -11,10 +11,21 @@ from html import unescape
 
 nb_questions_per_batch = 50
 
+nb_questions_written = 0
+
 # Get the list of all categories
 response = urlopen("https://opentdb.com/api_category.php")
 html = response.read()
 categories = json.loads(html)["trivia_categories"]
+
+# Find out about each category
+print("CATEGORIES:")
+for category in categories:
+    response = urlopen("https://opentdb.com/api_count.php?category=%d" % category["id"])
+    html = response.read()
+    nb_questions = json.loads(html)["category_question_count"]["total_question_count"]
+    category["nb_questions"] = nb_questions
+    print("%d %s (%d questions)" % (category["id"], category["name"], nb_questions))
 
 # Generate a token
 response = urlopen("https://opentdb.com/api_token.php?command=request")
@@ -26,12 +37,8 @@ print("Using session token: " + session_token)
 def download_category(category, token=session_token):
     category_id = category["id"]
 
-    # Get the number of questions for this category
-    response = urlopen("https://opentdb.com/api_count.php?category=%d" % category_id)
-    html = response.read()
-    nb_questions = json.loads(html)["category_question_count"]["total_question_count"]
-
     # Calculate the number of batches and the number of remaining questions
+    nb_questions = category["nb_questions"]
     nr_batches = floor(nb_questions / nb_questions_per_batch)
     remaining_questions = nb_questions % nb_questions_per_batch
 
@@ -40,17 +47,16 @@ def download_category(category, token=session_token):
 
     # Full-size batches
     for i in range(nr_batches):
-        print("Batch", i + 1, "of", nr_batches + 1, "in category", category_id)
+        print("Category %d: %d/%d" % (category_id, i + 1, nr_batches + 1))
         questions += next_batch(nb_questions_per_batch, category_id, token)
 
     # Last few questions
-    print(
-        "Batch", nr_batches + 1, "of", nr_batches + 1, "in category", category_id,
-    )
+    print("Category %d: %d/%d" % (category_id, nr_batches + 1, nr_batches + 1))
     questions += next_batch(remaining_questions, category_id, token)
 
-    # Process the questions/answers
-    write_questions(category["id"], questions)
+    category["questions"] = questions
+
+    return category
 
 
 def next_batch(nb_questions, category_id, token):
@@ -68,10 +74,6 @@ def next_batch(nb_questions, category_id, token):
 
 
 def write_categories(filename, categories):
-    print("Got the following categories:")
-    for category in categories:
-        print(category["id"], category["name"])
-
     file = open(filename, "w")
     for category in categories:
         file.write(
@@ -82,15 +84,19 @@ def write_categories(filename, categories):
     file.close()
 
 
-def write_questions(category_id, questions):
+def write_questions(category):
+    # Get the components we need
+    category_id = category["id"]
+    questions = category["questions"]
+    global nb_questions_written
+
     # Create the category file
     filename = "%d.lua" % category_id
     file = open(filename, "w")
 
-    count = 0
     for q in questions:
         if q["type"] != "boolean":  # skip all true/false questions
-            count += 1
+            nb_questions_written += 1
             out_dict = {
                 "Question": "[[" + unescape(q["question"]) + "]]",
                 "Answers": "{[[" + unescape(q["correct_answer"]) + "]]}",
@@ -99,7 +105,10 @@ def write_questions(category_id, questions):
                 "Hints": "{[[%s]]}" % (options_string(q)),
             }
             for k, v in out_dict.items():
-                file.write("TriviaBot_Questions[1]['%s'][%d] = %s;\n" % (k, count, v))
+                file.write(
+                    "TriviaBot_Questions[1]['%s'][%d] = %s;\n"
+                    % (k, nb_questions_written, v)
+                )
             file.write("\n")
     file.close
 
@@ -124,8 +133,12 @@ def difficulty_to_points(string):
 
 
 if __name__ == "__main__":
-    # categories = [categories[0], categories[1]]  # for testing
+    #categories = [categories[0], categories[1], categories[2]]  # for testing
     write_categories("categories.lua", categories)
     nbProcesses = len(categories)
     with Pool(nbProcesses) as p:
-        p.map(download_category, categories)
+        categories = list(p.map(download_category, categories))
+
+    # Write the questions/answers
+    for category in categories:
+        write_questions(category)
